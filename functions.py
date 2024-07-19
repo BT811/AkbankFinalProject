895f953449be4d23f71047428a71c174356f901e
Berk Türk <99087700+BT811@users.noreply.github.com>
2024-07-19 22:27:55 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,8 +1,18 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 from HisseTahmin import train_and_predict_next_n_days
+import mplfinance as mpf
+from graphFuntions import res_sup_line_graph, stock_graph, show_user_opertation_graph
+from myStocksFunctions import stock_information
+import pandas as pd
 
-ticker = None
+global ticker
+ticker=None
+global fig 
+fig=None
+global data 
+data=None
+
 
 def hello():
     return "Merhaba!"
@@ -12,32 +22,31 @@ def goodby():
 
 def enter_ticker():
     global ticker
+    global fig 
+    global data
+    fig = None
     ticker = input("Hisse senedi kodunu giriniz:")
-    if yf.Ticker(ticker).history(period ="1y").empty:
+    data = yf.Ticker(ticker).history(period ="1y")
+    if data.empty:
         print(f"{ticker} sembolü için veri bulunamadı. Tekrar deneyin")
-        enter_ticker()   
+        enter_ticker() 
+    
+    
+     
 
 def graph():
-    global ticker
+    global ticker  # Ticker sembolü büyük harflerle yazılmalı
+    global data
     if ticker == None:
         enter_ticker()
-        
-    data = yf.Ticker(ticker).history(period="1y")
-    data = yf.Ticker(ticker).history(period="1y")
-    plt.figure(figsize=(10,5))
-    plt.plot(data.index, data.Close)
-    plt.title(f"{ticker} Hisse Senedi Fiyatı (1 Yıllık)")
-    plt.xlabel("Tarih")
-    plt.ylabel("Fiyat(Dolar)")
-    plt.grid(True)
-    
-    return plt.gcf()
+
+    return stock_graph(data)
 
 def calculate_RSI():
     global ticker
+    global data
     if ticker == None:
         enter_ticker()
-    data = yf.Ticker(ticker).history(period="1y")
     delta = data['Close'].diff()
     up = delta.clip(lower=0)
     down = -1 * delta.clip(upper=0)
@@ -51,30 +60,32 @@ def calculate_RSI():
 
 def calculate_EMA():
     global ticker
+    global data
     if ticker == None:
         enter_ticker()
     window = int(input("Kaç günlük Üssel Hareketli Ortalamayı (EMA) görmek istiyorsunuz? "))
-    data = yf.Ticker(ticker).history(period="1y")
     ema = data['Close'].ewm(span=window, adjust=False).mean().iloc[-1]
     return str(ema)
 
 def calculate_SMA():
     global ticker
+    global data
     if ticker == None:
         enter_ticker()
     window = int(input("Son kaç günün kapanış fiyat ortalamasını (SMA) istiyorsunuz?"))
-    data = yf.Ticker(ticker).history(period="1y")
+
     sma = data['Close'].rolling(window=window).mean().iloc[-1]
 
     return str(sma)
 
 def calculate_MACD():
     global ticker
+    global data
     if ticker == None:
         enter_ticker()
-    data = yf.Ticker(ticker).history(period="1y")['Close']
-    short_EMA = data.ewm(span=12, adjust=False).mean()
-    long_EMA = data.ewm(span=26, adjust=False).mean()
+    df = data['Close']
+    short_EMA = df.ewm(span=12, adjust=False).mean()
+    long_EMA = df.ewm(span=26, adjust=False).mean()
 
     MACD = short_EMA - long_EMA
     signal = MACD.ewm(span=9, adjust=False).mean()
@@ -93,3 +104,26 @@ def model_tahmin():
         result_text += f"{i+1}. günün tahmin edilen açılış fiyatı: {price}\n"
 
     return result_text
+
+def res_sup_graph():
+    global ticker
+    global data
+
+    if ticker is None:
+        enter_ticker()
+    
+    
+    return res_sup_line_graph(data)
+    
+def myStocks():
+    user_operations = pd.read_csv("kullanıcı_işlemleri.csv")
+    return stock_information(user_operations)
+
+def user_opertation_graph():
+    global ticker
+    global data
+    if ticker is None:
+        enter_ticker()
+    user_operations = pd.read_csv("kullanıcı_işlemleri.csv", parse_dates=['İşlem Tarihi'], index_col='İşlem Tarihi')
+
+    return show_user_opertation_graph(ticker, data, user_operations)
